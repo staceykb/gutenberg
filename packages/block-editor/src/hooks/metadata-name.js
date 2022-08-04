460ee2bfc57f5dafd4d0689f0a1056f2518af723
Dave Smith <getdavemail@gmail.com>
2022-08-04 11:25:46 +0100

    Remove experimental code

--- packages/block-editor/src/hooks/metadata-name.js
+++ packages/block-editor/src/hooks/metadata-name.js
@@ -3,18 +3,6 @@
  */
 import { addFilter } from '@wordpress/hooks';
 import { hasBlockMetadataSupport } from '@wordpress/blocks';
-import { createHigherOrderComponent } from '@wordpress/compose';
-import { __ } from '@wordpress/i18n';
-import { MenuItem } from '@wordpress/components';
-
-/**
- * Internal dependencies
- */
-import BlockSettingsMenuControls from '../components/block-settings-menu-controls';
-
-/**
- * Internal dependencies
- */
 
 /**
  * Filters registered block settings, adding an `__experimentalLabel` callback if one does not already exist.
@@ -44,50 +32,8 @@ export function addLabelCallback( settings ) {
 	return settings;
 }
 
-export const withOptionsMenuItem = createHigherOrderComponent(
-	( BlockEdit ) => ( props ) => {
-		const { name: blockName, isSelected } = props;
-
-		const supportsBlockNaming = hasBlockMetadataSupport(
-			blockName,
-			'name'
-		);
-
-		return (
-			<>
-				{ isSelected && (
-					<BlockSettingsMenuControls>
-						{ () => (
-							<MenuItem
-								disabled={ ! supportsBlockNaming }
-								onClick={ () => {
-									// eslint-disable-next-line no-console
-									console.log(
-										`can't toggle editing from this context`
-									);
-									//toggleLabelEditingMode( true );
-								} }
-							>
-								{ __( 'Rename' ) }
-							</MenuItem>
-						) }
-					</BlockSettingsMenuControls>
-				) }
-				<BlockEdit { ...props } />
-			</>
-		);
-	},
-	'withOptionsMenuItem'
-);
-
 addFilter(
 	'blocks.registerBlockType',
 	'core/metadata/addLabelCallback',
 	addLabelCallback
 );
-
-// addFilter(
-// 	'editor.BlockEdit',
-// 	'core/editor/align/with-toolbar-controls',
-// 	withOptionsMenuItem
-// );
